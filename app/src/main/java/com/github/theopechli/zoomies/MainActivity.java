fa64b0de2f8a49e050096eaf66789b9de87f7fbd
Theofilos Pechlivanis <49034471+theopechli@users.noreply.github.com>
2021-01-22 12:59:07 +0200

    Add a button and create an intent to GetTrendsActivity

--- app/src/main/java/com/github/theopechli/zoomies/MainActivity.java
+++ app/src/main/java/com/github/theopechli/zoomies/MainActivity.java
@@ -2,15 +2,22 @@ package com.github.theopechli.zoomies;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String TAG="MainActivity";
+    private String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void getTrends(View view) {
+        Intent intent = new Intent(this, GetTrendsActivity.class);
+        startActivity(intent);
     }
 }
