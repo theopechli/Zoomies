b179bb89fddc3f4e0a825ded4a23150ee93028d5
Theofilos Pechlivanis <49034471+theopechli@users.noreply.github.com>
2021-01-23 14:56:22 +0200

    Move intent GetTrendsActivity to TrendsActivity

--- app/src/main/java/com/github/theopechli/zoomies/MainActivity.java
+++ app/src/main/java/com/github/theopechli/zoomies/MainActivity.java
@@ -14,10 +14,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void getTrends(View view) {
-        Intent intent = new Intent(this, GetTrendsActivity.class);
+        //TODO Twitter authentication
+        Intent intent = new Intent(this, TrendsActivity.class);
         startActivity(intent);
     }
 }
