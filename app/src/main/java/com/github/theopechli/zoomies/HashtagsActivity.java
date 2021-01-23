bfcd1f779a768ef715508ee5a64be0c46816c818
Theofilos Pechlivanis <49034471+theopechli@users.noreply.github.com>
2021-01-24 00:00:20 +0200

    Add a click listener to get the trends

--- app/src/main/java/com/github/theopechli/zoomies/HashtagsActivity.java
+++ app/src/main/java/com/github/theopechli/zoomies/HashtagsActivity.java
@@ -1,26 +1,98 @@
 package com.github.theopechli.zoomies;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+
+import twitter4j.Trend;
+import twitter4j.Trends;
+import twitter4j.TwitterException;
 
 public class HashtagsActivity extends AppCompatActivity {
+
+    private final String TAG = "HashtagsActivity";
+
+    RecyclerView recyclerView;
+    RecyclerView.Adapter trendsAdapter;
+    RecyclerView.LayoutManager layoutManager;
+    ArrayList<String> trendsList = new ArrayList<>();
+    int[] smnLogos = {R.drawable.ic_twitter, R.drawable.ic_facebook, R.drawable.ic_instagram};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_trends);
+        setContentView(R.layout.activity_hashtags);
+
+        final Button button = findViewById(R.id.btnGetTrends);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // FIXME remove deprecated AsyncTask
+                new HashtagsActivity.RetrieveTrendsTask(trendsList).execute();
+
+                recyclerView = findViewById(R.id.rvTrends);
+                recyclerView.setHasFixedSize(false);
+                layoutManager = new LinearLayoutManager(HashtagsActivity.this);
+                recyclerView.setLayoutManager(layoutManager);
+            }
+        });
     }
 
-    public void getTrends(View view) {
-        Intent intent = new Intent(this, GetTrendsActivity.class);
-        startActivity(intent);
-    }
+    private class RetrieveTrendsTask extends AsyncTask<Void, Void, ArrayList<String>> {
 
-    public void searchHashtags(View view) {
-        Intent intent = new Intent(this, SearchHashtagsActivity.class);
-        startActivity(intent);
+        // FIXME remove unnecessary reference
+        private WeakReference<ArrayList<String>> mTrendsList;
+
+        RetrieveTrendsTask(ArrayList<String> arrayList) {
+            mTrendsList = new WeakReference<>(arrayList);
+        }
+
+        @Override
+        protected ArrayList<String> doInBackground(Void... voids) {
+            ArrayList<String> arrayList = new ArrayList<>();
+
+            try {
+                int woeid = 1;
+                TwitterInstance twitter = new TwitterInstance();
+                Trends trends = twitter.getTwitterInstance().getPlaceTrends(woeid);
+                Log.i(TAG, "Showing trends for " + trends.getLocation().getName());
+
+                for (Trend trend : trends.getTrends()) {
+                    arrayList.add(trend.getName());
+                }
+
+                Log.i(TAG, "doInBackground done.");
+
+                return arrayList;
+            } catch (TwitterException te) {
+                te.printStackTrace();
+                Log.i(TAG, "Failed to get trends: " + te.getMessage());
+            } catch (NumberFormatException nfe) {
+                nfe.printStackTrace();
+                Log.i(TAG, "WOEID must be number");
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<String> result) {
+            super.onPostExecute(result);
+            if (result != null) {
+                mTrendsList.get().addAll(result);
+            }
+            trendsAdapter = new TrendsAdapter(HashtagsActivity.this, trendsList, smnLogos);
+            recyclerView.setAdapter(trendsAdapter);
+
+            Log.i(TAG, "onPostExecute done.");
+        }
     }
 }
