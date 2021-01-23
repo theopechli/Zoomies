665f30084f808283fd711ba0816643cf0d2dc982
Theofilos Pechlivanis <49034471+theopechli@users.noreply.github.com>
2021-01-23 14:59:49 +0200

    Add intents to GetTrendsActivity, SearchHashtagsActivity

--- app/src/main/java/com/github/theopechli/zoomies/TrendsActivity.java
+++ app/src/main/java/com/github/theopechli/zoomies/TrendsActivity.java
@@ -2,7 +2,9 @@ package com.github.theopechli.zoomies;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class TrendsActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class TrendsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trends);
+    }
+
+    public void getTrends(View view) {
+        Intent intent = new Intent(this, GetTrendsActivity.class);
+        startActivity(intent);
+    }
+
+    public void searchHashtags(View view) {
+        Intent intent = new Intent(this, SearchHashtagsActivity.class);
+        startActivity(intent);
     }
 }
