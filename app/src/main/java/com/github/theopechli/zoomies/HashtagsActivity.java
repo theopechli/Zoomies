126963c01c3d6f8c622e431537327737b4ea5934
Theofilos Pechlivanis <49034471+theopechli@users.noreply.github.com>
2021-01-27 19:38:17 +0200

    Move GetTrendsTask to TwitterInstance

--- app/src/main/java/com/github/theopechli/zoomies/HashtagsActivity.java
+++ app/src/main/java/com/github/theopechli/zoomies/HashtagsActivity.java
@@ -46,7 +46,7 @@ public class HashtagsActivity extends AppCompatActivity {
         final Button btnGetTrends = findViewById(R.id.btnGetTrends);
         btnGetTrends.setOnClickListener(v -> {
             // FIXME remove deprecated AsyncTask
-            new GetTrendsTask(trendsList).execute();
+            twitter.new GetTrendsTask(trendsAdapter, trendsList).execute();
         });
 
         final Button btnSearch = findViewById(R.id.btnSearch);
@@ -83,54 +83,6 @@ public class HashtagsActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(List<twitter4j.Status> tweets) {
             Log.i(TAG, "onPostExecute done.");
-        }
-    }
-
-    private class GetTrendsTask extends AsyncTask<Void, Void, ArrayList<String>> {
-
-        private WeakReference<ArrayList<String>> mTrendsList;
-
-        GetTrendsTask(ArrayList<String> arrayList) {
-            mTrendsList = new WeakReference<>(arrayList);
-        }
-
-        @Override
-        protected ArrayList<String> doInBackground(Void... voids) {
-            Log.i(TAG, "GetTrendsTask doInBackground start.");
-
-            ArrayList<String> arrayList = new ArrayList<>();
-
-            try {
-                int woeid = 1;
-                Trends trends = twitter.getTwitterInstance().getPlaceTrends(woeid);
-
-                Log.i(TAG, "Showing trends for " + trends.getLocation().getName());
-
-                for (Trend trend : trends.getTrends()) {
-                    arrayList.add(trend.getName());
-                }
-            } catch (TwitterException te) {
-                te.printStackTrace();
-                Log.i(TAG, "Failed to get trends: " + te.getMessage());
-            } catch (NumberFormatException nfe) {
-                nfe.printStackTrace();
-                Log.i(TAG, "WOEID must be number");
-            }
-
-            Log.i(TAG, "GetTrendsTask doInBackground done.");
-
-            return arrayList;
-        }
-
-        @Override
-        protected void onPostExecute(ArrayList<String> result) {
-            if (result != null) {
-                mTrendsList.get().addAll(result);
-            }
-
-            trendsAdapter.notifyDataSetChanged();
-
-            Log.i(TAG, "GetTrendsTask onPostExecute done.");
         }
     }
 }
--- app/src/main/java/com/github/theopechli/zoomies/TwitterInstance.java
+++ app/src/main/java/com/github/theopechli/zoomies/TwitterInstance.java
@@ -1,6 +1,16 @@
 package com.github.theopechli.zoomies;
 
+import android.os.AsyncTask;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+
+import twitter4j.Trend;
+import twitter4j.Trends;
 import twitter4j.Twitter;
+import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 import twitter4j.conf.ConfigurationBuilder;
 
@@ -22,5 +32,45 @@ public class TwitterInstance {
 
     public Twitter getTwitterInstance() {
         return twitter;
+    }
+
+    class GetTrendsTask extends AsyncTask<Void, Void, ArrayList<String>> {
+
+        private RecyclerView.Adapter trendsAdapter;
+        private WeakReference<ArrayList<String>> mTrendsList;
+
+        GetTrendsTask(RecyclerView.Adapter adapter, ArrayList<String> arrayList) {
+            trendsAdapter = adapter;
+            mTrendsList = new WeakReference<>(arrayList);
+        }
+
+        @Override
+        protected ArrayList<String> doInBackground(Void... voids) {
+            ArrayList<String> arrayList = new ArrayList<>();
+
+            try {
+                int woeid = 1;
+                Trends trends = twitter.getPlaceTrends(woeid);
+
+                for (Trend trend : trends.getTrends()) {
+                    arrayList.add(trend.getName());
+                }
+            } catch (TwitterException te) {
+                te.printStackTrace();
+            } catch (NumberFormatException nfe) {
+                nfe.printStackTrace();
+            }
+
+            return arrayList;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<String> result) {
+            if (result != null) {
+                mTrendsList.get().addAll(result);
+            }
+
+            trendsAdapter.notifyDataSetChanged();
+        }
     }
 }
