a84b047e29537c4657a4f17fe563c9f2df14ee37
Theofilos Pechlivanis <49034471+theopechli@users.noreply.github.com>
2021-01-28 18:55:03 +0200

    Get intent from TrendsAdapter, fill in the main body similarly to HashtagsActivity and move SearchPostsTask from HashtagsActivity

--- app/src/main/java/com/github/theopechli/zoomies/PostsActivity.java
+++ app/src/main/java/com/github/theopechli/zoomies/PostsActivity.java
@@ -1,14 +1,80 @@
 package com.github.theopechli.zoomies;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Status;
+import twitter4j.TwitterException;
 
 public class PostsActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter postsAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private List<twitter4j.Status> postsList = new ArrayList<>();
+    private int[] smnLogos = {R.drawable.ic_twitter, R.drawable.ic_facebook, R.drawable.ic_instagram};
+    private TwitterInstance twitterInstance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_posts);
+
+        Intent intent = getIntent();
+        String hashtag = intent.getStringExtra("com.github.theopechli.zoomies.hashtag");
+
+        if (DataHolder.getInstance().getTwitterInstance() == null) {
+            twitterInstance = new TwitterInstance();
+            DataHolder.getInstance().setTwitterInstance(twitterInstance);
+        } else {
+            twitterInstance = DataHolder.getInstance().getTwitterInstance();
+        }
+
+        recyclerView = findViewById(R.id.rvPosts);
+        recyclerView.setHasFixedSize(false);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        postsAdapter = new PostsAdapter(this, postsList, smnLogos);
+        recyclerView.setAdapter(postsAdapter);
+
+        new SearchPostsTask().execute(hashtag);
+    }
+
+    private class SearchPostsTask extends AsyncTask<String, Void, List<Status>> {
+
+        @Override
+        protected List<twitter4j.Status> doInBackground(String... params) {
+            List<twitter4j.Status> tweets = null;
+            try {
+                Query query = new Query(params[0]);
+                QueryResult result;
+                result = twitterInstance.getTwitter().search(query);
+                if (result != null) {
+                    tweets = result.getTweets();
+                }
+            } catch (TwitterException te) {
+                te.printStackTrace();
+            }
+            return tweets;
+        }
+
+        @Override
+        protected void onPostExecute(List<twitter4j.Status> tweets) {
+            if (tweets != null) {
+                postsList.addAll(tweets);
+                postsAdapter.notifyDataSetChanged();
+            }
+        }
     }
 }
