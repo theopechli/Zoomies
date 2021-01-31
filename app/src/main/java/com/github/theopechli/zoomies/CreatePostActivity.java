1e068a58b57c45bee35ef6e65ef3958b2437e760
Theofilos Pechlivanis <49034471+theopechli@users.noreply.github.com>
2021-01-31 14:37:54 +0200

    Get intent and initialize twitterInstance

--- app/src/main/java/com/github/theopechli/zoomies/CreatePostActivity.java
+++ app/src/main/java/com/github/theopechli/zoomies/CreatePostActivity.java
@@ -1,14 +1,29 @@
 package com.github.theopechli.zoomies;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class CreatePostActivity extends AppCompatActivity {
+
+    private TwitterInstance twitterInstance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_post);
+
+        getIntent();
+
+        if (DataHolder.getInstance().getTwitterInstance() == null) {
+            twitterInstance = new TwitterInstance();
+            DataHolder.getInstance().setTwitterInstance(twitterInstance);
+        } else {
+            twitterInstance = DataHolder.getInstance().getTwitterInstance();
+        }
+
+
     }
 }
